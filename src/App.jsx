366f8bda983edb3ef86676495acbfa16565c8b29
Sreeja A <156006746+OddEyed45@users.noreply.github.com>
2025-09-24 20:21:03 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+  const [multiplier, setMult] = useState(1)
+
+  const updateCount = () => { setCount(count + multiplier) }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className='header'>
+        <h1>Samosa Selector</h1>
+        <h2>Count: {count}</h2>
+        <img className='samosa' src='.\src\assets\samosa-img.png' onClick={updateCount}></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='container'>
+        <div className='upgrade'>
+          <h3>Double Stuffed 👯‍♀️</h3>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
